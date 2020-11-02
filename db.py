dedfccf49bf14dbc79a7f3c713c8b283164fdfc4
Felix <felix.meyer97@web.de>
2020-11-02 14:19:57 +0100

    added db.py data is stored in MongoDB

---
+++ db.py
@@ -0,0 +1,41 @@
+import datetime
+import pymongo
+
+try:
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    print("Connected successfully")
+except:
+    print("Couldnt connect to MongoDB")
+
+
+
+db = client.database
+
+collection = db.products
+#db = client["amazon"]
+
+def add_product_detail(details):
+    new = db["products"]
+    # extract ASIN from URL
+    ASIN = details["url"][len(details["url"])-10:len(details["url"])]
+
+    # try to store the data in DB
+    try:
+        details["date"] = datetime.datetime.now()
+        new.update_one({"asin":ASIN}, {"$set": {"asin":ASIN}, "$push": {"details":details}}, upsert=True)
+        return True
+    except Exception as identifier:
+        print(identifier)
+        return False
+
+def get_product_history(asin):
+    new = db["products"]
+    try:
+        find = new.find_one({"asin": asin}, {"_id": 0})
+        if find:
+            return find
+        else:
+            raise Exception("Not found")
+    except Exception as identifier:
+        print(identifier)
+        return None
--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 import scraper
+import db
+
+def run(url):
+    details = scraper.get_product_details(url)
+    result = ""
+    if details is None:
+        result = "not done"
+    else:
+        inserted = db.add_product_detail(details)
+        print(details)
+        if inserted:
+            result = "done"
+        else:
+            result = "not done"
+    return result
+
 
 if __name__ == '__main__':
-    #print(scraper.get_url("https://www.amazon.de/dp/B08C5HYHYB/ref=twister_B08FCQFLGD?_encoding=UTF8&psc=1"))
-    #print(scraper.get_price("121,69 €"))
-    print(scraper.get_product_details("https://www.amazon.de/dp/B08C5HYHYB/ref=twister_B08FCQFLGD?_encoding=UTF8&psc=1"))
+    URL = "https://www.amazon.de/dp/B08C5HYHYB/"
+
+    print(run(URL))
+
+
+
